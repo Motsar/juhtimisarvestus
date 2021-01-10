a6fc81101f00a1032841fa50b03c2a276669813a
martin.motsar <martin.motsar@khk.ee>
2021-01-10 17:29:00 +0200

    Adding a user to database

--- src/app.js
+++ src/app.js
@@ -1,21 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const session = require('express-session');
-const hbs  = require('express-handlebars');
-const passport = require('passport');
-const env = require('dotenv').config();
-require('dotenv').config({path:'../src/.env'});
+const bodyParser = require('body-parser');
+require('dotenv').config({path:'./src/.env'});
 
 const app = express();
-
-app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/' }));
-app.set('view engine', 'hbs');
-
-app.use(session({
-    resave: false,
-    saveUninitialized: true,
-    secret: 'SECRET'
-}));
+app.use(bodyParser.json());
 
 //Import routes
 const Users = require('./routes/users');
@@ -33,53 +22,11 @@ mongoose.connect(process.env.DB_CONNECT,
 
 //Route middlewares
 app.use('/', Users);
+
+
 app.use(express.json());
-app.get('/', function(req, res) {
-    res.render('auth', {layout: false});
-});
 
 //Setup serer
 app.listen(3000, ()=>{
     console.log('Server is up and running');
 })
-
-// Passport setup
-
-let userProfile;
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('/success', (req, res) => res.render('success', { user: userProfile } ));
-app.get('/error', (req, res) => res.send('Viga sisse logimisel'));
-
-passport.serializeUser(function(user, cb) {
-    cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-    cb(null, obj);
-})
-
-// Google auth
-
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-
-passport.use(new GoogleStrategy({
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/callback"
-    },
-    function (accessToken, refreshToken, profile, done) {
-        userProfile = profile;
-        return done(null, userProfile);
-    }
-));
-
-app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
-app.get('/auth/google/callback',
-    passport.authenticate('google', {failureRedirect: '/error'}),
-    function (req, res) {
-        // Successful authentification, redirect success
-        res.redirect('/success');
-});
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,33 @@
-const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const router = require('express').Router();
+const User = require('../models/User')
 
 router.post('/users',async(req,res) => {
+    console.log(req.body)
     //Checking if the user is already in database
     const emailExists = await User.findOne({email: req.body.email});
-    if(emailExists) return res.status(401).json({error: "Email allready exists!"});
+    if(emailExists) return res.status(401).json({error: "Email already exists!"});
+
+    //Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create a new user
+    const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: hashedPassword
+    });
+
+
+    try{
+        const savedUser = await user.save();
+        res.status(201).json({user: user._id});
+    }catch(err){
+        res.json({error:err});
+    }
+
 })
 
 module.exports = router;
